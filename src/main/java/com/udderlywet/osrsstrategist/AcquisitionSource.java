d78d34b271252bfdd83e8f7bfb39c67171a8707b
GiggyCash <shocker.llanos_60@icloud.com>
2026-08-16 14:11:37 +0100

    Add account-aware resource acquisition foundation

---
+++ src/main/java/com/udderlywet/osrsstrategist/AcquisitionSource.java
@@ -0,0 +1,14 @@
+package com.udderlywet.osrsstrategist;
+
+/**
+ * Where Strategist believes a required resource should come from.
+ */
+public enum AcquisitionSource
+{
+    INVENTORY,
+    BANK,
+    GROUP_STORAGE,
+    GRAND_EXCHANGE,
+    SELF_SOURCE,
+    CHECK_NEEDED
+}
