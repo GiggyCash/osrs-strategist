0e668eecd2d7259846ab9994e61861d71e06f306
GiggyCash <shocker.llanos_60@icloud.com>
2026-08-16 19:23:00 +0100

    Add optional restricted account build detection

---
+++ src/main/java/com/udderlywet/osrsstrategist/RestrictedBuildType.java
@@ -0,0 +1,13 @@
+package com.udderlywet.osrsstrategist;
+
+/** Optional player-confirmed account restrictions, separate from Jagex account mode. */
+public enum RestrictedBuildType
+{
+    STANDARD,
+    SKILLER,
+    ONE_DEFENCE_PURE,
+    DEFENCE_PURE,
+    ZERKER,
+    TEN_HITPOINTS,
+    F2P_SKILLER
+}
