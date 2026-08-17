99f39bc52f5c5b4e5f2db0459b7fb46eb371c717
GiggyCash <shocker.llanos_60@icloud.com>
2026-08-17 07:43:13 +0100

    Expand restricted account build types

--- src/main/java/com/udderlywet/osrsstrategist/RestrictedBuildType.java
+++ src/main/java/com/udderlywet/osrsstrategist/RestrictedBuildType.java
@@ -1,13 +1,28 @@
 package com.udderlywet.osrsstrategist;
 
-/** Optional player-confirmed account restrictions, separate from Jagex account mode. */
+/**
+ * Optional account-build restrictions layered on top of the Jagex account mode.
+ *
+ * <p>These describe player-imposed stat restrictions. They are deliberately
+ * separate from Main/Ironman/UIM/Hardcore/GIM because any of those account
+ * modes can also be a pure or skiller.</p>
+ */
 public enum RestrictedBuildType
 {
     STANDARD,
     SKILLER,
+    PRAYER_SKILLER,
+    F2P_SKILLER,
     ONE_DEFENCE_PURE,
-    DEFENCE_PURE,
+    LOW_DEFENCE_PURE,
+    INITIATE_PURE,
+    RUNE_PURE,
+    VOID_PURE,
     ZERKER,
+    OBSIDIAN_MAULER,
+    RANGE_TANK,
+    MED_BUILD,
+    DEFENCE_PURE,
     TEN_HITPOINTS,
-    F2P_SKILLER
+    COMBAT_ONLY
 }
